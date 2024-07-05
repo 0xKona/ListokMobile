1e7eee6fd6acd4f2aa7dc6f09b02729a44a9eb2b
0xKona <konarobinson@outlook.com>
2024-07-05 15:53:06 +0100

    Recipe Editor Navigation Implemented

---
+++ app/components/recipe-manager/recipe-editor/recipe-editor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RecipeEditor = () => {
+  return (
+    <View>
+      <Text>RecipeEditor</Text>
+    </View>
+  );
+};
+
+export default RecipeEditor;
--- app/components/recipe-manager/recipe-manager-home/recipe-manager-header.tsx
+++ app/components/recipe-manager/recipe-manager-home/recipe-manager-header.tsx
@@ -4,16 +4,20 @@ import SearchBar from '../../ui/search';
 import ListokButton from '../../ui/button';
 import useTheme from '../../hooks/useTheme';
 import { ThemeType } from '@app/constants/themes';
+import { useNavigation } from '@react-navigation/native';
+import { RecipeNavigationProp } from '@typed/navigation';
 
 const RecipeHeader = () => {
   const componentStyles = useTheme(styles);
+  const navigation = useNavigation<RecipeNavigationProp>();
 
   return (
     <View style={componentStyles.container}>
       <SearchBar />
       <ListokButton
-        onPress={() => {}}
+        onPress={() => navigation.navigate('New Recipe')}
         text="New Recipe"
+        // eslint-disable-next-line react-native/no-inline-styles
         propStyles={{ width: '30%', height: '100%' }}
       />
     </View>
