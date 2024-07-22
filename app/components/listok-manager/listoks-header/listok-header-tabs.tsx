0f2c6bcf554437b8413eb0d5ced6d741324a6e7f
0xKona <konarobinson@outlook.com>
2024-07-22 16:31:17 +0100

    add tabs and new listok button

---
+++ app/components/listok-manager/listoks-header/listok-header-tabs.tsx
@@ -0,0 +1,57 @@
+import useTheme from '@app/components/hooks/useTheme';
+import { ThemeType } from '@app/constants/themes';
+import React from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import {
+  ListokManagerTabInterface,
+  ListokManagerTabs,
+} from '../listok-manager';
+
+interface PropsInterface {
+  currentTab: ListokManagerTabInterface;
+  handlePressTab: (tab: ListokManagerTabInterface) => void;
+}
+
+const ListokTabs = ({ currentTab, handlePressTab }: PropsInterface) => {
+  const componentStyle = useTheme(styles);
+  const selectedStyle = (selected: boolean) => {
+    return selected
+      ? {
+          ...componentStyle.tab,
+          borderBottomWidth: 2,
+          borderBottomColor: 'black',
+        }
+      : componentStyle.tab;
+  };
+
+  return (
+    <View style={componentStyle.container}>
+      {ListokManagerTabs.map((tab: any) => (
+        <TouchableHighlight
+          style={selectedStyle(currentTab.value === tab.value)}
+          onPress={() => handlePressTab(tab)}
+          key={tab.value}>
+          <Text>{tab.name}</Text>
+        </TouchableHighlight>
+      ))}
+    </View>
+  );
+};
+
+const styles = (props: ThemeType) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      height: 50,
+    },
+    tab: {
+      backgroundColor: props.surface,
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 10,
+      minWidth: 100,
+    },
+  });
+
+export default ListokTabs;
