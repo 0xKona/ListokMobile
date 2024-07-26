5f7bb49d01d3ac7a05b64449bd05b083647d5d08
0xKona <konarobinson@outlook.com>
2024-07-26 13:46:24 +0100

    add close button for add ingredient

--- app/components/recipe-manager/recipe-editor/recipe-ingredients.tsx/ingredient-form.tsx
+++ app/components/recipe-manager/recipe-editor/recipe-ingredients.tsx/ingredient-form.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ListokInput from '@app/components/ui/input';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import { CategoryType, MeasurementType } from '@typed/recipe-types';
 import {
   ingredientCategoryOptions,
   ingredientMeasurementOptions,
 } from '@app/constants/ingredients';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { ThemeType } from '@app/constants/themes';
+import useTheme from '@app/components/hooks/useTheme';
 
-const IngredientForm = ({ addNewIngredient }: any) => {
+const IngredientForm = ({ addNewIngredient, closeForm }: any) => {
   const [ingredientName, setIngredientName] = React.useState('');
   const [ingredientAmount, setIngredientAmount] = React.useState<number>(0);
   const [ingredientMeasurement, setIngredientMeasurement] =
     React.useState<MeasurementType>('units');
   const [ingredientCategory, setIngredientCategory] =
     React.useState<CategoryType>('fruit');
+
+  const theme = useTheme(styles);
 
   const handleSubmit = () => {
     addNewIngredient({
@@ -25,44 +30,66 @@ const IngredientForm = ({ addNewIngredient }: any) => {
     });
   };
 
-  return (
-    <View style={styles.container}>
-      <Text>Add New Ingredient</Text>
-      <Text>Ingredient Name</Text>
-      <ListokInput value={ingredientName} onChangeText={setIngredientName} />
-      <Text>Ingredient Amount</Text>
-      <ListokInput
-        value={ingredientAmount}
-        onChangeText={setIngredientAmount}
-        type="number"
-      />
-      <Text>Amount Measurement</Text>
-      <RNPickerSelect
-        onValueChange={(value: any) => setIngredientMeasurement(value)}
-        items={ingredientMeasurementOptions}
-        value={ingredientMeasurement}
-        itemKey={ingredientMeasurement}
-      />
-      <Text>Ingredient Category</Text>
-      <RNPickerSelect
-        onValueChange={(value: any) => setIngredientCategory(value)}
-        items={ingredientCategoryOptions}
-        value={ingredientCategory}
-        itemKey={ingredientCategory}
-      />
+  const handleClose = () => {
+    closeForm();
+    setIngredientName('');
+    setIngredientAmount(0);
+    setIngredientMeasurement('units');
+    setIngredientCategory('fruit');
+  };
 
-      <View>
-        <Button title="Submit" onPress={handleSubmit} />
+  return (
+    <>
+      <View style={theme.closeButtonContainer}>
+        <TouchableOpacity onPress={handleClose}>
+          <Icon name="closecircle" size={20} />
+        </TouchableOpacity>
       </View>
-    </View>
+
+      <View style={theme.container}>
+        <Text>Add New Ingredient</Text>
+        <Text>Ingredient Name</Text>
+        <ListokInput value={ingredientName} onChangeText={setIngredientName} />
+        <Text>Ingredient Amount</Text>
+        <ListokInput
+          value={ingredientAmount}
+          onChangeText={setIngredientAmount}
+          type="number"
+        />
+        <Text>Amount Measurement</Text>
+        <RNPickerSelect
+          onValueChange={(value: any) => setIngredientMeasurement(value)}
+          items={ingredientMeasurementOptions}
+          value={ingredientMeasurement}
+          itemKey={ingredientMeasurement}
+        />
+        <Text>Ingredient Category</Text>
+        <RNPickerSelect
+          onValueChange={(value: any) => setIngredientCategory(value)}
+          items={ingredientCategoryOptions}
+          value={ingredientCategory}
+          itemKey={ingredientCategory}
+        />
+
+        <View>
+          <Button title="Submit" onPress={handleSubmit} />
+        </View>
+      </View>
+    </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    width: '100%',
-  },
-});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const styles = (theme: ThemeType) =>
+  StyleSheet.create({
+    container: {
+      display: 'flex',
+      width: '100%',
+    },
+    closeButtonContainer: {
+      width: '100%',
+      alignItems: 'flex-end',
+    },
+  });
 
 export default IngredientForm;
--- app/components/recipe-manager/recipe-editor/recipe-ingredients.tsx/recipe-ingredients.tsx
+++ app/components/recipe-manager/recipe-editor/recipe-ingredients.tsx/recipe-ingredients.tsx
@@ -4,11 +4,13 @@ import {
   updateRecipeIngredients,
 } from '@redux/slices/recipeEditorSlice';
 import { IngredientType } from '@typed/recipe-types';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import IngredientList from './ingredient-list';
 import IngredientForm from './ingredient-form';
+import { ThemeType } from '@app/constants/themes';
+import useTheme from '@app/components/hooks/useTheme';
 
 interface PropsType {
   recipeData: RecipeEditorState;
@@ -18,11 +20,17 @@ const RecipeIngredientsEditor = ({ recipeData }: PropsType) => {
   const [ingredients, setIngredients] = React.useState(
     recipeData.recipeData.ingredients,
   );
+
+  useEffect(() => {
+    setIngredients(recipeData.recipeData.ingredients);
+  }, [recipeData]);
+
   const [openIngredientForm, setOpenIngredientForm] = React.useState(false);
 
   const noIngredients = ingredients.length === 0;
   console.log(recipeData);
   const dispatch = useDispatch();
+  const theme = useTheme(styles);
 
   const openIngredient = () => {
     setOpenIngredientForm(true);
@@ -44,38 +52,75 @@ const RecipeIngredientsEditor = ({ recipeData }: PropsType) => {
   };
 
   return (
-    <View style={styles.container}>
-      {openIngredientForm ? (
-        <IngredientForm addNewIngredient={addNewIngredient} />
-      ) : (
-        <>
-          <Text>Ingredients</Text>
-          {noIngredients ? (
-            <Text>No Ingredients</Text>
-          ) : (
-            <IngredientList ingredients={ingredients} />
-          )}
-          <Button title="Add New" onPress={openIngredient} />
-          <View style={styles.buttonContainer}>
-            <Button title="Back" onPress={handleBack} />
-            <Button title="Next" onPress={handleNext} />
-          </View>
-        </>
-      )}
-    </View>
+    <>
+      <View style={theme.container}>
+        {openIngredientForm ? (
+          <IngredientForm
+            addNewIngredient={addNewIngredient}
+            closeForm={() => setOpenIngredientForm(false)}
+          />
+        ) : (
+          <>
+            <Text style={theme.titleText}>Ingredients</Text>
+            {noIngredients ? (
+              <Text>No Ingredients</Text>
+            ) : (
+              <IngredientList ingredients={ingredients} />
+            )}
+            <Button title="Add New" onPress={openIngredient} />
+          </>
+        )}
+      </View>
+      <View style={theme.buttonContainer}>
+        <Button title="Back" onPress={handleBack} />
+        <Button title="Next" onPress={handleNext} />
+      </View>
+    </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-});
+const styles = (theme: ThemeType) =>
+  StyleSheet.create({
+    container: {
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: theme.surface,
+      flexGrow: 1,
+      padding: 20,
+      margin: 20,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginTop: 'auto',
+      marginBottom: 20,
+    },
+    titleText: {
+      alignSelf: 'center',
+      fontSize: 20,
+      marginBottom: 20,
+    },
+  });
+
+// const styles = (theme: ThemeType) =>
+//   StyleSheet.create({
+//     container: {
+//       backgroundColor: theme.surface,
+//       padding: 20,
+//       margin: 20,
+//     },
+//     titleText: {
+//       alignSelf: 'center',
+//       fontSize: 20,
+//       marginBottom: 20,
+//     },
+//     inputSection: {
+//       marginTop: 10,
+//     },
+//     buttonContainer: {
+//       marginTop: 'auto',
+//       marginBottom: 20,
+//     },
+//   });
 
 export default RecipeIngredientsEditor;
