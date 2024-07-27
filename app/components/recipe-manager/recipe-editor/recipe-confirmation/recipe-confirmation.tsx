23bdaa526bdabbb51943a2b9a62ec24ec6c1738f
0xKona <konarobinson@outlook.com>
2024-07-27 12:26:54 +0100

    styling update for recipe form

--- app/components/recipe-manager/recipe-editor/recipe-confirmation/recipe-confirmation.tsx
+++ app/components/recipe-manager/recipe-editor/recipe-confirmation/recipe-confirmation.tsx
@@ -4,8 +4,10 @@ import { changeCurrentStep } from '@redux/slices/recipeEditorSlice';
 import { RootState } from '@redux/store';
 import { RecipeNavigationProp } from '@typed/navigation';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ScrollView } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import { ThemeType } from '@app/constants/themes';
+import useTheme from '@app/components/hooks/useTheme';
 
 const RecipeEditorConfirmation = ({ recipeData, setIgnorePopup }: any) => {
   const navigation = useNavigation<RecipeNavigationProp>();
@@ -13,6 +15,7 @@ const RecipeEditorConfirmation = ({ recipeData, setIgnorePopup }: any) => {
   console.log('User State Token at Confirmation Screen:', user.token);
   console.log('RecipeData.existing: ', recipeData.existingRecipe);
   const dispatch = useDispatch();
+  const theme = useTheme(styles);
 
   const handleBack = () => {
     dispatch(changeCurrentStep(3));
@@ -52,15 +55,76 @@ const RecipeEditorConfirmation = ({ recipeData, setIgnorePopup }: any) => {
   };
 
   return (
-    <View>
-      <Text>Confirmation Screen</Text>
-
-      <View>
+    <ScrollView style={theme.container}>
+      <Text style={theme.titleText}>Confirm</Text>
+      <View style={theme.section}>
+        <Text style={theme.label}>Title:</Text>
+        <Text style={theme.value}>{recipeData.recipeData.title}</Text>
+      </View>
+      <View style={theme.section}>
+        <Text style={theme.label}>Description:</Text>
+        <Text style={theme.value}>{recipeData.recipeData.desc}</Text>
+      </View>
+      <View style={theme.section}>
+        <Text style={theme.label}>Created By:</Text>
+        <Text style={theme.value}>{recipeData.recipeData.createdByName}</Text>
+      </View>
+      <View style={theme.section}>
+        <Text style={theme.label}>Ingredients:</Text>
+        {recipeData.recipeData.ingredients.map(
+          (ingredient: any, index: number) => (
+            <Text key={index} style={theme.value}>
+              {ingredient.name} - {ingredient.amount} {ingredient.measurement}
+            </Text>
+          ),
+        )}
+      </View>
+      <View style={theme.section}>
+        <Text style={theme.label}>Method:</Text>
+        {recipeData.recipeData.method.map((step: any, index: number) => (
+          <Text key={index} style={theme.value}>
+            {index + 1}. {step.step}
+          </Text>
+        ))}
+      </View>
+      <View style={theme.buttonContainer}>
         <Button title="Back" onPress={handleBack} />
         <Button title="Submit Recipe" onPress={handleSubmit} />
       </View>
-    </View>
+    </ScrollView>
   );
 };
+
+const styles = (theme: ThemeType) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: theme.surface,
+    },
+    titleText: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      alignSelf: 'center',
+    },
+    section: {
+      marginBottom: 15,
+    },
+    label: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      // color: theme.primary,
+    },
+    value: {
+      fontSize: 16,
+      // color: theme.text,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginTop: 20,
+    },
+  });
 
 export default RecipeEditorConfirmation;
