4570709af5d89ccad46e935bb07c811fe6e59872
Connor Robinson <connorrobinson@Connors-MacBook-Pro.local>
2024-08-17 18:33:51 +0100

    setup infastructure for listok login later

---
+++ app/components/login-form/listok-login/listok-login.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+// TODO - Implement Listok Login
+
+const ListokLogin = () => {
+    const [showCreateForm, setShowCreateForm] = useState<boolean>(false);
+
+    return (
+        null
+    )
+}
+
+export default ListokLogin;
