4d2207bc1833ded0b7640074ab71bad5f87624fb
0xKona <konarobinson@outlook.com>
2024-08-31 15:28:16 +0100

    recipes home styled

--- app/components/recipe-manager/recipe-manager-home/recipe-list.tsx
+++ app/components/recipe-manager/recipe-manager-home/recipe-list.tsx
@@ -1,28 +1,40 @@
 import useTheme from '@app/components/hooks/useTheme';
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import RecipeCard from './recipe-card';
 import { useSelector } from 'react-redux';
 import { RootState } from '@redux/store';
 import { RecipeType } from '@typed/recipe-types';
 import EmptyRecipeList from './empty-recipe-list';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 interface PropsType {
+  currentTab: string;
   refreshRecipes: () => void;
 }
 
-const RecipeList = ({ refreshRecipes }: PropsType) => {
-  const componentStyle = useTheme(styles);
+const RecipeList = ({ currentTab, refreshRecipes }: PropsType) => {
+  const theme = useTheme(styles);
   const { userRecipes, loading } = useSelector(
     (state: RootState) => state.recipeManager,
   );
+
+  // TODO : Implement favorites
+  if (currentTab === 'favorites') {
+    return (
+      <View style={theme.construction}>
+        <Icon name="construction" size={100} />
+        <Text style={theme.constructionText} >{"Imma be real dawg, \n I just haven't had time to do this yet"}</Text>
+      </View>
+    )
+  }
 
   return (
     <FlatList
       refreshing={loading}
       onRefresh={refreshRecipes}
       extraData={loading}
-      style={componentStyle.container}
+      style={theme.container}
       data={userRecipes}
       renderItem={({ item }) => (
         <RecipeCard data={item} refreshRecipes={refreshRecipes} />
@@ -38,6 +50,15 @@ const styles = () =>
     container: {
       width: '100%',
     },
+    construction: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexGrow: 1,
+      backgroundColor: 'white'
+    },
+    constructionText: {
+      textAlign: 'center'
+    }
   });
 
 export default RecipeList;
--- app/components/ui/action-sheet.tsx
+++ app/components/ui/action-sheet.tsx
@@ -1,4 +1,4 @@
-import { ActionSheetIOS } from "react-native";
+import { ActionSheetIOS, Platform } from "react-native";
 import { store } from '@redux/store'
 
 export interface ActionInterface {
@@ -11,22 +11,27 @@ export const actionSheet = (
     destructiveIndex?: number
 ) => {
 
-    const actionNames = ['Cancel', ...actions.map((action: ActionInterface) => action.actionName)];
-    const actionFuncs = [() => {}, ...actions.map((action: ActionInterface) => action.actionFunction)];
-
-    const checkDarkMode = () => store.getState().theme.currentTheme.themeName === 'dark'
-
-    const destructiveButtonIndex = destructiveIndex ? destructiveIndex : null;
-
-    ActionSheetIOS.showActionSheetWithOptions(
-        {
-            options: actionNames,
-            destructiveButtonIndex: destructiveButtonIndex,
-            cancelButtonIndex: 0,
-            userInterfaceStyle: checkDarkMode() ? 'dark' : 'light'
-        },
-        buttonIndex => {
-            actionFuncs[buttonIndex]();
-        }    
-    )
+    if (Platform.OS === 'ios') {
+        const actionNames = ['Cancel', ...actions.map((action: ActionInterface) => action.actionName)];
+        const actionFuncs = [() => {}, ...actions.map((action: ActionInterface) => action.actionFunction)];
+    
+        const checkDarkMode = () => store.getState().theme.currentTheme.themeName === 'dark'
+    
+        const destructiveButtonIndex = destructiveIndex ? destructiveIndex : null;
+    
+        ActionSheetIOS.showActionSheetWithOptions(
+            {
+                options: actionNames,
+                destructiveButtonIndex: destructiveButtonIndex,
+                cancelButtonIndex: 0,
+                userInterfaceStyle: checkDarkMode() ? 'dark' : 'light'
+            },
+            buttonIndex => {
+                actionFuncs[buttonIndex]();
+            }    
+        )
+    } else {
+        // TODO : Implement non iOS usage.
+        console.error("Action sheet currently implemented for iOS only, if refactoring for android use, implement in the action-sheet file")
+    }
 }
