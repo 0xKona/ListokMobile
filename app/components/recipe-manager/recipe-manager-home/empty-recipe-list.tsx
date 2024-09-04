31b17d0331dbf49c3e3106240fdca467bce7f5e1
0xKona <konarobinson@outlook.com>
2024-09-04 18:25:57 +0100

    fix dark mode styling for recipes flatlist

--- app/components/recipe-manager/recipe-manager-home/empty-recipe-list.tsx
+++ app/components/recipe-manager/recipe-manager-home/empty-recipe-list.tsx
@@ -1,22 +1,28 @@
+import useTheme from '@app/components/hooks/useTheme';
+import { ThemeType } from '@app/constants/themes';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const EmptyRecipeList = () => {
+
+  const theme = useTheme(styles)
+
   return (
-    <View style={styles.container}>
+    <View style={theme.container}>
       <Text>You have no recipes</Text>
       <Text>(pull down to try refreshing)</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+const styles = (theme: ThemeType) => StyleSheet.create({
   container: {
-    flexGrow: 1,
-    height: 200,
+    // flexGrow: 1,
+    height: '100%',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: theme.surface
   },
 });
 
