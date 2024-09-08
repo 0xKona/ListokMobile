e28114131df5cb521a31c60df57b104878f66405
0xKona <konarobinson@outlook.com>
2024-09-08 13:41:24 +0100

    listok card styling

--- app/components/listok-manager/listok-list/listok-card.tsx
+++ app/components/listok-manager/listok-list/listok-card.tsx
@@ -1,4 +1,5 @@
 import useTheme from '@app/components/hooks/useTheme';
+import { ActionInterface, actionSheet } from '@app/components/ui/action-sheet';
 import { ThemeType } from '@app/constants/themes';
 import { listokManagerApis } from '@app/utils/api-connections/listok-manager-api';
 import { useNavigation } from '@react-navigation/native';
@@ -9,7 +10,9 @@ import {
 import { RootState } from '@redux/store';
 import { ListokNavigationProp } from '@typed/navigation';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { useDispatch, useSelector } from 'react-redux';
 
 interface PropsInterface {
@@ -22,12 +25,25 @@ const ListokCard = ({ data, refreshListoks }: PropsInterface) => {
 
   const { token } = useSelector((state: RootState) => state.user.user);
   const dispatch = useDispatch();
-  const componentStyles = useTheme(styles);
+  const theme = useTheme(styles);
+
+  const swipeableRef = React.useRef<any>(null);
+  const closeSwipeable = () => {
+    if (swipeableRef.current) {
+      swipeableRef.current.close();
+    }
+  }
 
   const handleDeleteListok = () => {
+    closeSwipeable();
     listokManagerApis.deleteListok(data.id, token);
     refreshListoks();
   };
+
+  const confirmDelete = () => {
+    const actions: ActionInterface[] = [{actionName: 'Delete this recipe', actionFunction: handleDeleteListok}];
+    actionSheet(actions, 1, closeSwipeable);
+  }
 
   const openListok = () => {
     dispatch(resetListokEditor());
@@ -35,27 +51,44 @@ const ListokCard = ({ data, refreshListoks }: PropsInterface) => {
     navigation.navigate('New Listok');
   };
 
+  const renderRightAction = () => (
+    <TouchableOpacity style={{height: 75, aspectRatio: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red'}} onPress={confirmDelete}>
+      <Icon name='trash-alt' size={20} color={'white'}/>
+    </TouchableOpacity>
+  )
+
   return (
-    <View style={componentStyles.container}>
-      <Text>{data.title}</Text>
-      <Text>{data.desc}</Text>
-      {/* eslint-disable-next-line react-native/no-inline-styles */}
-      <View style={{ flexDirection: 'row' }}>
-        <Button title="Delete Listok" onPress={() => handleDeleteListok()} />
-        <Button title="Open Listok" onPress={openListok} />
+    <Swipeable ref={swipeableRef} renderRightActions={renderRightAction} overshootRight={false} >
+      <View style={theme.container}>
+        <View style={theme.titleContainer} >
+          <Text style={theme.titleContainerText}>{data.title}</Text>
+          <Text style={theme.titleContainerText}>{data.desc}</Text>
+        </View>
+        {/* eslint-disable-next-line react-native/no-inline-styles */}
+        <TouchableOpacity style={{ marginLeft: 'auto', height: 75, aspectRatio: 1, justifyContent: 'center', alignItems: 'center'}} onPress={openListok} >
+          <Icon name='edit' size={20} color={theme.titleContainerText.color}/>
+        </TouchableOpacity>
       </View>
-    </View>
+    </Swipeable>
   );
 };
 
-const styles = (props: ThemeType) =>
+const styles = (theme: ThemeType) =>
   StyleSheet.create({
     container: {
       width: '100%',
-      backgroundColor: props.surface,
+      backgroundColor: theme.surface,
       marginBottom: 2,
       height: 75,
-      padding: 10,
+      flexDirection: 'row',
+    },
+    titleContainer: {
+      height: '100%',
+      justifyContent: 'center',
+      padding: 20,
+    },
+    titleContainerText: {
+      color: theme.surfaceText
     },
   });
 
